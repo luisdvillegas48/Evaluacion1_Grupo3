b390ed7062d8ab1e17eeb9edd9b98167aa89020e
Geremi <duransamuel544@gmail.com>
2026-06-03 12:42:43 -0400

    Agregar codigo main y Dockerfile

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    printf("Bienvenido al sistema del Grupo 3\n");
+    
+    char nombre[50];
+    printf("Ingresa tu nombre: ");
+    scanf("%s", nombre);
+    
+   
+    printf("Hola %s, gracias por usar el programa.\n", nombre);
+    printf("--------------------------------\n");
+    
+   
+    printf("Integrantes del equipo:\n");
+    printf("- Luis Villegas\n");
+    printf("- Jariel Velasco\n");
+    printf("- Xiomara Salinas\n");
+    printf("- Deiri parede luna\n");
+    printf("- Geremi Duran\n");
+    printf("- Alvys Paredes\n");
+    
+    return 0;
+}
